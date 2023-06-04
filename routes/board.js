555bbdf95772a0a58aa1bea3ebed566104b66344
esakiryota <esaki1217@gmail.com>
2023-06-04 11:42:58 +0900

    feat: board機能の追加

---
+++ routes/board.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/:roomId', function(req, res, next) {
+  var user = req.query;
+  var user_name =  generateRandomString(15);
+  params = {"token": req.params.roomId, "user_name": user_name, "user": user, "image": ""}
+  res.render('board', params);
+});
+
+function generateRandomString(length) {
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+    return result;
+  }
+  
+
+module.exports = router;

fe52b15cf13aa1df2af38cce9765ed9ea3d38ccb
esakiryota <esaki1217@gmail.com>
2023-06-07 23:26:11 +0900

    feat: wss対応や微修正、localstorage設定

--- routes/board.js
+++ routes/board.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/:roomId', function(req, res, next) {
   var user = req.query;
   var user_name =  generateRandomString(15);
-  params = {"token": req.params.roomId, "user_name": user.name, "user": user, "image": ""}
+  params = {"token": req.params.roomId, "user_name": user_name, "user": user, "display_name": user.name, "image": ""}
+  console.log(params)
   res.render('board', params);
 });
 
